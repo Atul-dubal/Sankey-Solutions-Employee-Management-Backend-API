5740f31e8ef94c62b2036bb87eb3b6ad7aa2764b
Atul Dubal <atuldubal199@gmail.com>
2026-02-13 14:09:30 +0530

    Sankey Solutions Employee Management Backend API

--- config/db.js
+++ config/db.js
@@ -1,16 +1,37 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL, {
     dialect: "postgres",
+    protocol: "postgres",
     logging: false,
-    protocol: "postgres"
-  }
-);
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+}
+
+module.exports = sequelize;
+
 
 module.exports = sequelize;
